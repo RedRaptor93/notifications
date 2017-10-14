978301900ca93b4ecbb27555f7f65db9d2b1a115
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2017-10-14 19:16:44 -0300

    updated samples

--- Samples/Samples.Core/App.cs
+++ Samples/Samples.Core/App.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using Plugin.Notifications;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 
 namespace Samples
@@ -14,81 +11,7 @@ namespace Samples
 
         public App()
         {
-            Notification.DefaultTitle = "Test Title";
-            var btnPermission = new Button {Text = "Request Permission"};
-            btnPermission.Command = new Command(async () =>
-            {
-                var result = await CrossNotifications.Current.RequestPermission();
-                btnPermission.Text = result ? "Permission Granted" : "Permission Denied";
-            });
-
-            this.MainPage = new NavigationPage(new ContentPage
-            {
-                Title = "Notifications",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children =
-                    {
-                        btnPermission,
-                        new Button
-                        {
-                            Text = "Set Badge",
-                            Command = new Command(() => CrossNotifications.Current.SetBadge(new Random().Next(100)))
-                        },
-                        new Button
-                        {
-                            Text = "Clear Badge",
-                            Command = new Command(() => CrossNotifications.Current.SetBadge(0))
-                        },
-                        new Button
-                        {
-                            Text = "Press This & Exit App within 10 seconds",
-                            Command = new Command(() =>
-                                CrossNotifications.Current.Send(new Notification
-                                {
-                                    Title = "HELLO!",
-                                    Message = "Hello from the ACR Sample Notification App",
-                                    Vibrate = true,
-                                    When = TimeSpan.FromSeconds(10)
-                                })
-                            )
-                        },
-                        new Button
-                        {
-                            Text = "Multiple Timed Messages (10 messages x 5 seconds apart)",
-                            Command = new Command(() =>
-                            {
-                                CrossNotifications.Current.Send(new Notification
-                                {
-                                    Title = "Samples",
-                                    Message = "Starting Sample Schedule Notifications"
-                                });
-                                for (var i = 1; i < 11; i++)
-                                {
-                                    var seconds = i * 5;
-                                    var id = CrossNotifications.Current.Send(new Notification
-                                    {
-                                        Message = $"Message {i}",
-                                        When = TimeSpan.FromSeconds(seconds)
-                                    });
-                                    Debug.WriteLine($"Notification ID: {id}");
-                                }
-                            })
-                        },
-                        new Button
-                        {
-                            Text = "Cancel All Notifications",
-                            Command = new Command(() => CrossNotifications.Current.CancelAll())
-                        },
-                        new Button
-                        {
-                            Text = "Vibrate",
-                            Command = new Command(() => CrossNotifications.Current.Vibrate())
-                        }
-                    }
-                }
-            });
+            this.MainPage = new NavigationPage(new MainPage());
         }
 
 
---
+++ Samples/Samples.Core/MainPage.cs
@@ -0,0 +1,153 @@
+﻿using Plugin.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Samples
+{
+    class MainPage : ContentPage
+    {
+        static readonly string SEP = new string('-', 30);
+
+        ScrollView root;
+        StackLayout layout;
+
+        Button btnPermission, btnSetBadge, btnClearBadge,
+            btnTestNotifDefIcon, btnTestNotifCustomIcon,
+            btnMultipleMsgs, btnCancelAll, btnVibrate;
+
+        Label lblTestNotif, lblBadges, lblSep;
+
+        private void InitComponents()
+        {
+            root = new ScrollView();
+
+            btnPermission = new Button
+            {
+                Text = "Request Permission",
+                Command = new Command(async () =>
+                {
+                    var result = await CrossNotifications.Current.RequestPermission();
+                    btnPermission.Text = result ? "Permission Granted" : "Permission Denied";
+                })
+            };
+            lblBadges = new Label
+            {
+                Text = "Set and clear Badge numbers",
+                Margin = new Thickness(10, 5)
+            };
+            btnSetBadge = new Button
+            {
+                Text = "Set Badge",
+                Command = new Command(() => CrossNotifications.Current.SetBadge(new Random().Next(100)))
+            };
+            btnClearBadge = new Button
+            {
+                Text = "Clear Badge",
+                Command = new Command(() => CrossNotifications.Current.SetBadge(0))
+            };
+            lblTestNotif = new Label
+            {
+                Text = "Send notifications, Press the buttons below and exit App withing 10 seconds:",
+                Margin = new Thickness(10, 5)
+            };
+            btnTestNotifDefIcon = new Button
+            {
+                Text = "Notification w/ default icon",
+                Command = new Command(() =>
+                    CrossNotifications.Current.Send(new Notification
+                    {
+                        Title = "HELLO!",
+                        Message = "Hello from the ACR Sample Notification App, you should see the App's icon displayed",
+                        Vibrate = true,
+                        When = TimeSpan.FromSeconds(10),
+                    }))
+            };
+            btnTestNotifCustomIcon = new Button
+            {
+                Text = "Notification w/ custom icon",
+                Command = new Command(() =>
+                    CrossNotifications.Current.Send(new Notification
+                    {
+                        Title = "HELLO!",
+                        Message = "Hello from the ACR Sample Notification App, you should see a custom icon displayed",
+                        Vibrate = true,
+                        When = TimeSpan.FromSeconds(10),
+                        IconName = "ic_addcard"
+                    }))
+            };
+            lblSep = new Label
+            {
+                Text = SEP + "  Others  " + SEP,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            btnMultipleMsgs = new Button
+            {
+                Text = "Multiple Timed Messages (10 messages x 5 seconds apart)",
+                Command = new Command(() =>
+                {
+                    CrossNotifications.Current.Send(new Notification
+                    {
+                        Title = "Samples",
+                        Message = "Starting Sample Schedule Notifications"
+                    });
+                    for (var i = 1; i < 11; i++)
+                    {
+                        var seconds = i * 5;
+                        var id = CrossNotifications.Current.Send(new Notification
+                        {
+                            Message = $"Message {i}",
+                            When = TimeSpan.FromSeconds(seconds)
+                        });
+                        Debug.WriteLine($"Notification ID: {id}");
+                    }
+                })
+            };
+            btnCancelAll = new Button
+            {
+                Text = "Cancel All Notifications",
+                Command = new Command(() => CrossNotifications.Current.CancelAll())
+            };
+            btnVibrate = new Button
+            {
+                Text = "Vibrate",
+                Command = new Command(() => CrossNotifications.Current.Vibrate())
+            };
+
+            layout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+
+                Children =
+                {
+                    btnPermission,
+                    lblBadges,
+                    btnSetBadge,
+                    btnClearBadge,
+                    lblTestNotif,
+                    btnTestNotifDefIcon,
+                    btnTestNotifCustomIcon,
+                    lblSep,
+                    btnMultipleMsgs,
+                    btnCancelAll,
+                    btnVibrate
+                }
+            };
+
+            root.Content = layout;
+            this.Content = root;
+        } 
+
+
+        public MainPage()
+        {
+            InitComponents();
+            Title = "Notifications";
+            Notification.DefaultTitle = "Test Title";
+
+        }
+
+    }
+}
