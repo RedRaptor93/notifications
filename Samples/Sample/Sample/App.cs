1e3eaace68bf1bc386a9d7f7c4650604bc7e387a
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2017-11-13 12:16:18 -0200

    replaced sample files, they where giving random build errors, hope this fixes that

---
+++ Samples/Sample/Sample/App.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+
+namespace Sample
+{
+
+    public class App : Application
+    {
+        public static bool IsInBackgrounded { get; private set; }
+
+
+        public App()
+        {
+            this.MainPage = new NavigationPage(new MainPage());
+        }
+
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            App.IsInBackgrounded = false;
+        }
+
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            App.IsInBackgrounded = true;
+        }
+
+    }
+}
